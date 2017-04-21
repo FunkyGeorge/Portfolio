73edfb0218e8dd95d690935a0927be4d9e37b81c
George Miranda <George Miranda>
2017-04-21 09:50:19 -0700

    add dynamic filling iframes

--- static/js/main.js
+++ static/js/main.js
@@ -10,4 +10,28 @@ $(document).ready(function(){
 	},function(){
 		$(this).css('border-bottom','4px solid black');
 	});
+
+  $('.tab').click(function(){
+    var tabClicked = $(this).children().attr('href');
+    
+    switch (tabClicked){
+      case '#Evergreen':
+        $('#frame-evergreen').attr('src','http://evergreenmake.com');
+        break;
+      case '#Ronin':
+        $('#frame-ronin').attr('src','http://ronin.world');
+        break;
+      case '#GDND':
+        $('#frame-gdnd').attr('src','http://54.215.143.208');
+        break;
+      case '#Trucks':
+        $('#frame-foodtrucks').attr('src','http://foodtrucks.zaks.pw');
+        break;
+      default:
+    }
+
+
+  });
+
+
 });
