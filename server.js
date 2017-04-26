244fdf240fad2e3c82275853dfa12b67e96a220e
George Miranda <George Miranda>
2017-04-26 00:30:05 -0700

    add server

---
+++ server.js
@@ -0,0 +1,19 @@
+var	express  = require('express');
+var fs = require('fs');
+var path = require('path');
+
+
+
+var root = __dirname;
+var port = process.env.PORT || 8000;
+var app = express();
+
+
+
+app.use(express.static(path.join(root, 'client')));
+
+
+
+var server = app.listen(port, function() {
+	console.log(`server running on port ${ port }`);
+});
